b85d763082854c236d3550e21082e3052f4bec69
trond <trondm25@gmail.com>
2019-06-23 12:31:31 +0200

    commented out route to make site work

--- index.js
+++ index.js
@@ -13,17 +13,17 @@ mongo.connect("mongodb://heroku_29pgkl9j:8ab0uap50e3ecf5g34roi1ticr@ds139167.mla
     app.get('/', (req, res) => res.render('pages/index'))
     app.listen(PORT, () => console.log(`Listening on ${PORT}`))
 
-    router.post('/index', function(req, res) {
-    console.log(req.body.name);
-    console.log(req.body.message);
-    var newMsg = new Message();
-    newMsg.name = req.body.name;
-    newMsg.message = req.body.message;
-    newMsg.save(function(err, result) {
-    if(err){
-        throw err;
-    }
-    console.log('Message sent!')
-    res.send();
-    });
-    });
+    // router.post('/index', function(req, res) {
+    // console.log(req.body.name);
+    // console.log(req.body.message);
+    // var newMsg = new Message();
+    // newMsg.name = req.body.name;
+    // newMsg.message = req.body.message;
+    // newMsg.save(function(err, result) {
+    // if(err){
+    //     throw err;
+    // }
+    // console.log('Message sent!')
+    // res.send();
+    // });
+    // });
